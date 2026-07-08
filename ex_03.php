61a2ac141383d28c0bf68be81f8f81f028a7388a
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-07-08 10:54:49 -0300

    up - ex-03 completo

--- ex_03.php
+++ ex_03.php
@@ -2,20 +2,24 @@
 
 function mascararCpf($cpf){
 
-$cpfFragmentado = preg_split("//u", $cpf, -1,PREG_SPLIT_NO_EMPTY);
-
-$cpfDivido = substr_replace($cpfFragmentado, '.', -4, 0);
+$cpfDivido = substr_replace($cpf, '.', -4, 0);
 
 $esconderCpf = explode(".", $cpfDivido);
 
-$cpfEscondido = str_replace($esconderCpf, "*");
+$cpfEscondido = str_replace(["0","1","2","3","4","5","6","7","8","9"], "*", $esconderCpf[0]);
+
+$cpfOculto = $cpfEscondido;
+
+$cpfVisivel = $esconderCpf[1];
+
+$cpfPronto = $cpfOculto . $cpfVisivel;
+
+return $cpfPronto;
 
 }
 
-$cpf = 14083304901;
+$cpf = "14083304901";
 
-
-
-
+echo mascararCpf($cpf);
 
 ?>
