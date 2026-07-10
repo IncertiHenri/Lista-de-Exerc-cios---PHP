1fed79e0c78208c808a39e30c28120fa8d4c1385
IncertiHenri <henrique_incerti@estudante.sesisenai.org.br>
2026-07-10 12:24:37 -0300

    add - ex-07 inicio

---
+++ ex_07.php
@@ -0,0 +1,15 @@
+<?php
+/* Uma loja virtual oferece descontos conforme o valor da compra.
+Crie uma função chamada calcularDesconto() que receba o valor total da compra
+e aplique as seguintes regras:
+● Até R$ 100,00: sem desconto;
+● Acima de R$ 100,00: 10%;
+● Acima de R$ 500,00: 20%;
+● Acima de R$ 1.000,00: 30%.
+Retorne o valor original, o desconto aplicado e o valor final da compra */
+
+function calcularDesconto($valorCompra){
+
+}
+
+?>
