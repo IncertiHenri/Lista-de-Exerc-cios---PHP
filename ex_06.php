45338d8f08bbcc0b8a31c0c52731c830cdd90e14
IncertiHenri <henrique_incerti@estudante.sesisenai.org.br>
2026-07-10 10:50:43 -0300

    add - ex-06 final

--- ex_06.php
+++ ex_06.php
@@ -1,8 +1,43 @@
 <?php
 
+/* Uma empresa que fabrica sensores precisa converter temperaturas entre diferentes
+escalas.
+Crie uma função chamada converterTemperatura() que receba um valor, a escala
+de origem e a escala de destino.
+A função deverá permitir conversões entre Celsius, Fahrenheit e Kelvin. */
+
 function converterTemperatura($temperatura, $dadoInicial, $dadoFinal){
 
+    switch ($dadoInicial) {
+        case "Celsius":
+            switch ($dadoFinal) {
+                case "Fahrenheit":
+                    return ($temperatura * 9/5) + 32;
+                case "Kelvin":
+                    return $temperatura + 273.15;
+            }
+        case "Fahrenheit":
+            switch ($dadoFinal) {
+                case "Celsius":
+                    return ($temperatura - 32) * 5/9;
+                case "Kelvin":
+                    return ($temperatura - 32) * 5/9 + 273.15;
+            }
+        case "Kelvin":
+            switch ($dadoFinal) {
+                case "Celsius":
+                    return $temperatura - 273.15;
+                case "Fahrenheit":
+                    return ($temperatura - 273.15) * 9/5 + 32;
+            }
+    }
 
 }
+
+$temperatura = 24;
+$dadoInicial = "Celsius";
+$dadoFinal = "Fahrenheit";
+
+echo converterTemperatura($temperatura, $dadoInicial, $dadoFinal);
 
 ?>
